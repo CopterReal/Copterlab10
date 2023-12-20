27c34ee00de7a898c860117b5105c1e3ed3ce099
Copter <copter101x18@gmail.com>
2023-12-19 16:53:11 -0800

    Completed

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -6,19 +6,16 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
 	
 	string textline;
-	getline(source,textline);
 	dest << "-------------------- BOOM ---------------------\n";
-	dest << "AR CHI\n";
 	while(getline(source,textline)){
 		cout << textline << '\n';
 	}
 	getline(source,textline);
 	dest << "-------------------- HA!! ---------------------";
-	
 
     source.close();
     dest.close();
